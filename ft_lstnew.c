c6ffadabc466b19b4932f852e51f3143a241bf95
jAzzvdou <joaoazevedo592@gmail.com>
2023-10-25 00:56:40 -0300

    Libft

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 00:23:24 by jazevedo          #+#    #+#             */
+/*   Updated: 2023/10/25 00:50:09 by jazevedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_l;
+
+	new_l = malloc(sizeof(t_list));
+	if (new_l == NULL)
+		return (NULL);
+	new_l->content = content;
+	new_l->next = NULL;
+	return (new_l);
+}
