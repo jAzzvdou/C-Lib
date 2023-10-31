cdde5c06f1ebc283fa5d864799fa562371c6c999
jAzzvdou <joaoazevedo592@gmail.com>
2023-10-31 18:47:14 -0300

    Libft

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:37:15 by jazevedo          #+#    #+#             */
-/*   Updated: 2023/10/28 11:19:33 by jazevedo         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:46:31 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ void	ft_bzero(void *s, size_t n)
 		zero_s[n] = 0;
 }
 /*
-Essa função preenche N (n) casas de uma string (s) com NULL.
+Essa função preenche N (n) casas de uma string X (s) com NULL.
 */
